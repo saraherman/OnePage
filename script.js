b64286382fcc7c3e983cc549b4c04f4b4301ada7
Sara Herman <saraherman92@gmail.com>
2015-03-05 23:15:17 -0800

    OnePage
    My Portfolio site. mostly complete. Missing mailer, CONTENT, and slick
    hookup for second slider.

---
+++ script.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+	    }, 500);
+	    return false;
+	});
+
+	// initialize slick
+	$('.slick').slick({
+		
+	});
+	$('.single-item').slick();
+
+});
